33de2d68d536a69217b971481cf3a151b85ae68d
sandil2003 <it23657700@my.sliit.lk>
2025-12-08 04:59:03 +0530

    frontend internal folder structure

---
+++ moodz-frontend/src/app/pages/mood-agent/mood-agent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mood-agent',
+  imports: [],
+  templateUrl: './mood-agent.html',
+  styleUrl: './mood-agent.css',
+})
+export class MoodAgent {
+
+}
