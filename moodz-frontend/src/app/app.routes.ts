74e3bd6738ae28dddff2c225190b282a73a02565
sandil2003 <it23657700@my.sliit.lk>
2025-12-08 09:08:20 +0530

    initial home page

--- moodz-frontend/src/app/app.routes.ts
+++ moodz-frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home)
+    },
+    {
+        path: 'mood-agent',
+        loadComponent: () => import('./pages/mood-agent/mood-agent').then(m => m.MoodAgent)
+    }
+];
