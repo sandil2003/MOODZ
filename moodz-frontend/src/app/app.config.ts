23f81adeb5b91b149fc7371db42bd9597e959e7d
sandil2003 <it23657700@my.sliit.lk>
2025-12-10 15:27:38 +0530

    added ngx-markdown for deep search

--- moodz-frontend/src/app/app.config.ts
+++ moodz-frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideMarkdown } from 'ngx-markdown';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideMarkdown()
   ]
 };
