d3d5352a44d1d0148b2b5204ea5f83b3e6b7e0d0
sandil2003 <it23657700@my.sliit.lk>
2026-01-02 03:33:30 +0530

    implement the typing animation to the about us page

--- moodz-frontend/src/app/pages/about-us/about-us.ts
+++ moodz-frontend/src/app/pages/about-us/about-us.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,6 +7,32 @@ import { RouterLink } from '@angular/router';
   templateUrl: './about-us.html',
   styleUrl: './about-us.css'
 })
-export class AboutUs {
+export class AboutUs implements OnInit, OnDestroy {
+  typedText = signal<string>('');
+  isTypingComplete = signal<boolean>(false);
+  private fullText = 'Empowering Mental Wellness Through AI Innovation';
+  private typingInterval: any;
+  private currentIndex = 0;
 
+  ngOnInit() {
+    this.startTypingAnimation();
+  }
+
+  ngOnDestroy() {
+    if (this.typingInterval) {
+      clearInterval(this.typingInterval);
+    }
+  }
+
+  private startTypingAnimation() {
+    this.typingInterval = setInterval(() => {
+      if (this.currentIndex < this.fullText.length) {
+        this.typedText.set(this.fullText.substring(0, this.currentIndex + 1));
+        this.currentIndex++;
+      } else {
+        clearInterval(this.typingInterval);
+        this.isTypingComplete.set(true);
+      }
+    }, 50); // 50ms per character for smooth typing
+  }
 }
